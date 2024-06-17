01399bbe18ce85af7ef1221ebfeaff8bbdc4861f
kakamana <kamig4u@gmail.com>
2024-06-17 22:40:00 +0400

    portfolio with next js
    First check in for potfolio with next js. I have followed along JS Mastery youtube video

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7c75665e858d0b758e16f8ade4f0ec99@o4507435591991296.ingest.us.sentry.io/4507435600052224",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
